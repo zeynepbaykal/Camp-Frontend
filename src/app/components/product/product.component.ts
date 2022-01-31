3a11682da9ed2a9ddc941e9bbc4a09f7c7d7e44c
zeynepbaykal <55082321+zeynepbaykal@users.noreply.github.com>
2022-01-31 16:57:46 +0300

    17th day

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Product } from '../models/product';
+import { ProductResponseModel } from 'src/app/models/productResponseModel';
+import { ProductService } from 'src/app/services/product.service';
+
+import { Product } from '../../models/product';
 
 @Component({
   selector: 'app-product',
@@ -9,9 +12,20 @@ import { Product } from '../models/product';
 export class ProductComponent implements OnInit {
 
   products:Product[] = [];
-  constructor() { }
+  dataLoaded=false;
+ 
+  constructor(private productService:ProductService){ }
 
   ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts(){
+    this.productService.getProducts().subscribe(response=>{
+      this.products=response.data
+      this.dataLoaded=true;
+    })
+   
   }
 
 }
